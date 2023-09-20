b031536066e82116682dbc83802d15142d7416d9
Tyuiu.SamarAA@outlook.com <istore103@icloud.com>
2023-09-20 22:33:52 +0500

    Task1

---
+++ Tyuiu.SamarAA.Sprint1.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SamarAA.Sprint1.Task1.V4.Lib;
+
+namespace Tyuiu.SamarAA.Sprint1.Task1.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
