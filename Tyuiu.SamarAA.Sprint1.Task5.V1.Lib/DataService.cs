24e281327e0a5b3fb088e022ee038e52653a5ffd
Tyuiu.SamarAA@outlook.com <istore103@icloud.com>
2023-09-26 19:20:21 +0500

    Task5

---
+++ Tyuiu.SamarAA.Sprint1.Task5.V1.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SamarAA.Sprint1.Task5.V1.Lib
+{
+    public class DataService : ISprint1Task5V1
+    {
+        public int DistanceBetweenDots(double x1, double y1, double x2, double y2)
+        {
+            int res = Convert.ToInt32(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SamarAA.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SamarAA.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.SamarAA.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x1 = -1;
+            double x2 = 3;
+            double y1 = 6;
+            double y2 = 2;
+            DataService ds = new DataService();
+            double res = ds.DistanceBetweenDots(x1,x2,y1,y2);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 7;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
