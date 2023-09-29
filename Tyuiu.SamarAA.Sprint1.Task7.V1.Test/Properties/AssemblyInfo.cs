dcf74da2dc5d8cc0cba7a712a876abe07fc4dcd2
Tyuiu.SamarAA@outlook.com <istore103@icloud.com>
2023-09-29 19:17:36 +0500

    Task7

---
+++ Tyuiu.SamarAA.Sprint1.Task7.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SamarAA.Sprint1.Task7.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SamarAA.Sprint1.Task7.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("90bfa063-c8fb-4ace-a8e6-00a0b2bd68dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SamarAA.Sprint1.Task7.V1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SamarAA.Sprint1.Task7.V1.Lib;
+
+namespace Tyuiu.SamarAA.Sprint1.Task7.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Самар А. С. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Самар Андрей Александрович | ИИПб-23-3                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("b + Sqrt(b^2 + 4 * a * c ");
+            Console.WriteLine("--------------------------    - a^3 * c - b^-2");
+            Console.WriteLine("           2 * a         ");
+
+            Console.WriteLine("Введите координаты A:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты C:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(a, b, c));
+            Console.ReadKey();
+        }
+    }
+}
